179f081274a085f028c176ce43b50adaa2b3630b
bingoohuang <bingoo.huang@gmail.com>
2014-09-07 11:06:08 +0800

    refactorings

--- src/main/java/org/n3r/demo/AutoWireComponentJob.java
+++ src/main/java/org/n3r/demo/AutoWireComponentJob.java
@@ -2,7 +2,7 @@ package org.n3r.demo;
 
 import org.n3r.quartz.glass.configuration.Configuration;
 import org.n3r.quartz.glass.job.annotation.GlassJob;
-import org.n3r.quartz.glass.job.annotation.GlassTrigger;
+// import org.n3r.quartz.glass.job.annotation.GlassTrigger;
 import org.n3r.quartz.glass.job.annotation.JobArgument;
 import org.n3r.quartz.glass.log.joblog.JobLogs;
 import org.quartz.Job;
@@ -11,7 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-@GlassTrigger(scheduler = "Every 3 seconds", triggerDataMap = "name=冰果皇")
+// @GlassTrigger(scheduler = "Every 3 seconds", triggerDataMap = "name=冰果皇")
 @GlassJob(description = "Test job for autowired supporting",
         team = "火箭队", created = "2014-09-02")
 public class AutoWireComponentJob implements Job {
